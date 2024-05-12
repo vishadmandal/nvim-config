c2d25ed83f9668f2ee293305cc74279d0330a3bd
vishadmetawing <vishad.mandal@verismart.ai>
2024-05-12 17:19:48 +0530

    updated config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+--if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- Customize Mason plugins
 
@@ -12,6 +12,17 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "lua_ls",
+        "clangd",
+        "cssls",
+        "dockerls",
+        "jsonls",
+        "jdtls",
+        "tsserver",
+        "intelephense",
+        "basedpyright",
+        "ruby_ls",
+        "rust_analyzer",
+        "terraformls",
         -- add more arguments for adding more language servers
       })
     end,
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+--if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- Customize Treesitter
 
@@ -10,6 +10,16 @@ return {
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
       "lua",
       "vim",
+      "python",
+      "java",
+      "javascript",
+      "php",
+      "rust",
+      "c",
+      "cpp",
+      "css",
+      "terraform",
+      "dockerfile",
       -- add more arguments for adding more treesitter parsers
     })
   end,
